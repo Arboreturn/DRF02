67f44c0ed3c621d37ba0c1882692f35703479e3d
efecan <efecanozkoparan05@gmail.com>
2023-07-24 00:44:45 +0300

    project foundations laid

--- kitaplar/models.py
+++ kitaplar/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
+class Kitap(models.Model):
+    isim = models.CharField(max_length=150)
+    yazar = models.CharField(max_length=150)
+    aciklama = models.TextField(max_length=150,null=True,blank=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True) # bir kerelik gelecek biz değiştirmicez o yüzden otomatik yapsın kendisi
+    güncellenme_tarihi = models.DateField(auto_now_add=True)
+    yayın_tarihi = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.isim} - {self.yazar}'
+
+class Yorum(models.Model):
+    kitap= models.ForeignKey(Kitap,on_delete=models.CASCADE,related_name='yorumlar')
+    yorum_sahibi = models.CharField(max_length=255)
+    yorum = models.TextField(blank=True,null=True)
+    yaratilma_tarihi = models.DateTimeField(auto_now_add=True) # bir kerelik gelecek biz değiştirmicez o yüzden otomatik yapsın kendisi
+    güncellenme_tarihi = models.DateField(auto_now_add=True)
+
+    degerlendirme = models.PositiveIntegerField(
+        validators= [MinValueValidator(1),MaxValueValidator(5)],
+    )
+
+    def __str__(self):
+        return str(self.degerlendirme)
